f36656eaf237969764efb6653221cc9a255b4d91
Marek Radzik <mradzik454@gmail.com>
2020-10-10 15:00:07 +0200

    image preview

--- js/main.js
+++ js/main.js
@@ -71,5 +71,24 @@ jQuery.noConflict();
                 });
             });
         }
+
+        //questionnarie form - helper - image file preview
+        $('.image-file').change(function() {
+            readURL(this);
+        });
+
     });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          
+          reader.onload = function(e) {
+            $('#img_preview').attr('src', e.target.result);
+          }
+          
+          reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+      }
+
 }(jQuery));
